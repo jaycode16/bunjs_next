06ad55a5fccfe1d482d4c416abcc71907557049e
jaycode16 <joshuacode116@gmail.com>
2022-12-11 00:58:49 -0600

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({}) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> v
+          Welcome to Bunjs | <a href="https://github.com/oven-sh/bun#using-bun-with-nextjs"> Nextjs!</a> v
           {nextPackage.version}
         </h1>
 
